7b46f3ced0e68bdcb11685a9fea4ccb5998a3167
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2024-01-18 17:05:27 +0100

    fix Task01

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,6 +1,6 @@
 --SQL script create a users table
 CREATE TABLE users(
-    id INT NOT NULL AUTO_INCREMENT,
+    id INT AUTO_INCREMENT,
     email varchar(255) NOT NULL UNIQUE,
     name varchar(255),
     PRIMARY KEY (id)
