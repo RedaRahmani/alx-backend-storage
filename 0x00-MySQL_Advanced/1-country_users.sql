b82a07206fa4b66d9ea748b08c1f1592cfd49455
RedaRahmani <mohamedreda.rahmani20@ump.ac.ma>
2024-01-18 17:16:31 +0100

    Fix Task02

--- 0x00-MySQL_Advanced/1-country_users.sql
+++ 0x00-MySQL_Advanced/1-country_users.sql
@@ -1,7 +1,7 @@
 -- Sript that create users table 
 CREATE TABLE IF NOT EXISTS users(
-    id INT AUTO_INCREMENT,
-    email VARCHAR(255),
+    id INT NOT NULL AUTO_INCREMENT,
+    email VARCHAR(255) NOT NULL,
     name VARCHAR(255),
     country ENUM('US','CO','TN') NOT NULL?
     PRIMARY KEY (id)
